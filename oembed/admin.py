3b6ccc5e48f19547946ddb77fa611758f6e7eef0
floguy <floguy@4c08321d-174e-0410-8f1d-477679e35061>
2008-07-21 08:36:03 +0000

    Added admin registration for StoredOEmbed, since it can't really hurt.
    git-svn-id: https://django-oembed.googlecode.com/svn/trunk@17 4c08321d-174e-0410-8f1d-477679e35061

--- oembed/admin.py
+++ oembed/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from models import ProviderRule
+from models import ProviderRule, StoredOEmbed
 
 admin.site.register(ProviderRule)
+admin.site.register(StoredOEmbed)
