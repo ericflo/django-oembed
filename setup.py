329fee682b9d885b8d181b828bdab94a5232c335
floguy <floguy@4c08321d-174e-0410-8f1d-477679e35061>
2009-01-24 21:37:16 +0000

    Added a setup.py to django-oembed
    git-svn-id: https://django-oembed.googlecode.com/svn/trunk@29 4c08321d-174e-0410-8f1d-477679e35061

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-oembed',
+    version='0.1.0',
+    description='A collection of Django tools which make it easy to change text filled with oembed links into the embedded objects themselves.',
+    author='Eric Florenzano',
+    author_email='floguy@gmail.com',
+    url='http://django-oembed.googlecode.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
